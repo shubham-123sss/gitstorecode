bd4faa9cdd9ac180aec4588a2a84a1afa4f2b4aa
DELL <sbhujbal023@gmail.com>
2023-07-08 13:57:08 -1000

    push the code

---
+++ src/main/java/qa/pages/Homepage.java
@@ -0,0 +1,14 @@
+package qa.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class Homepage {
+
+	WebDriver driver;
+	public Homepage(WebDriver driver) {
+	
+	this.driver=driver;
+		
+	}
+	
+}
---
+++ src/main/java/qa/pages/Loginpage.java
@@ -0,0 +1,17 @@
+package qa.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class Loginpage {
+
+	WebDriver driver;
+	public Loginpage(WebDriver driver) {
+	
+	this.driver=driver;
+	
+	
+	}
+	
+	
+	
+}
